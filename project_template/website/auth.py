9c058eca883ab67dd215b14e90306ba580cbe1ae
juliasteed <jsteed@vols.utk.edu>
2022-09-24 14:45:43 -0400

    issue #14, added last name

--- project_template/website/auth.py
+++ project_template/website/auth.py
@@ -26,6 +26,7 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -33,17 +34,19 @@ def sign_up():
             flash('Email must be greater than 4 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 character', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
             #add user to database
-            new_user = User(email=email, first_name=first_name, password=password1)
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=password1)
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='sucess')
-            return redirect(url_for('views.events')) # redirect to home page after new account created
+            return redirect(url_for('views.events')) # redirect to events page after new account created
 
 
     return render_template("sign_up.html")
