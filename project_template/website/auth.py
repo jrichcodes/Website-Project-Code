5831daed567ce12acafec7ad3caae939ba63a74c
juliasteed <jsteed@vols.utk.edu>
2022-10-10 11:59:06 -0400

    made sign up redirect to login

--- project_template/website/auth.py
+++ project_template/website/auth.py
@@ -67,7 +67,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='sucess')
-            return redirect(url_for('views.events')) # redirect to events page after new account created
+            return redirect(url_for('auth.login')) # redirect to events page after new account created
 
 
     return render_template("sign_up.html", user=current_user)
