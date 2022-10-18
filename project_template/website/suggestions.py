511d128675c69936b515b5ea4ab5ef04bb8aaaa0
yejashi <befikirteferi@gmail.com>
2022-10-17 23:04:49 -0400

    Issue 25: progress

--- project_template/website/suggestions.py
+++ project_template/website/suggestions.py
@@ -1,10 +1,15 @@
 from gettext import find
-import requests, bs4, re
+import requests, bs4, re, shutil, time, json
 
 class trip:
     trip_name = None
     img = None
+    #TODO Africa
     description = None
+
+    def __init__(self, trip_name, img):
+        self.trip_name = trip_name
+        self.img = img
 
 class continent:
     name = None
@@ -12,6 +17,8 @@ class continent:
 
     def __init__(self, name):
         self.name = name
+
+world = {'Africa': {}, 'North America': {}, 'South America': {}, 'Asia': {}, 'Oceania': {}, 'Europe': {}}
 
 Africa = continent("Africa")
 North_America = continent("North America")
@@ -27,7 +34,9 @@ images = []
 req = requests.get('https://www.planetware.com/africa/best-places-to-visit-saf-1-36.htm')
 finder = bs4.BeautifulSoup(req.text, 'lxml')
 
+#Raw names
 x = finder.find_all('h2', class_="sitename")
+#Raw image links
 y = finder.find_all('img', class_='wpimg')
 
 #Gets all the names in proper format
@@ -36,3 +45,27 @@ for i in range(len(x)):
         names.append(x[i].text[3::])
     elif x[i].text[3] == ' ':
         names.append(x[i].text[4::])
+
+for i in y:
+    images.append("https://www.planetware.com" + i["src"])
+
+# for i in images:
+#     req = requests.get(i, stream=True)
+#     filename = "website/static/images/suggestions/" + i[72::]
+#     filename = open(filename, 'wb')
+#     req.raw.decode_content = True
+#     shutil.copyfileobj(req.raw, filename)
+#     filename.close
+#     time.sleep(0.5)
+
+for i in range(len(x)):
+    curr_trip = trip(names[i], "website/static/images/suggestions/" + images[i][72::])
+    Africa.trips.append(curr_trip)
+
+for i in Africa.trips:
+    world['Africa'][i.trip_name] = i.img
+
+
+with open('website/static/json_files/suggestions.json', 'w') as json_file:
+    json.dump(world, json_file, indent = 4)
+
