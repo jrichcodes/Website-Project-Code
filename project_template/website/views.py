bc5cb7b8cd00ac28da7461300bf5de02e68f7018
juliasteed <jsteed@vols.utk.edu>
2022-10-21 10:43:29 -0400

    trying to avoid merg conf

--- project_template/website/views.py
+++ project_template/website/views.py
@@ -15,19 +15,22 @@ def home():
 @login_required
 def events():
     if request.method == 'POST':
-        date_in = request.form.get('tripDate')
-        time_in = request.form.get('tripTime')
-        name_in = request.form.get('name')
-        desc_in = request.form.get('desc')
-        tripType_in = request.form.get('tripType')
-        date_time = datetime.strptime(date_in + " " + time_in,"%Y-%m-%d %H:%M")
-        if len(name_in) < 1:
-            flash('Trip name is too short!', category='error')
-        else:
-            new_trip = Trip(name = name_in, desc = desc_in, trip_type = tripType_in, date = date_time, user_id=current_user.id)
-            db.session.add(new_trip)
-            db.session.commit()
-            flash('Trip added!', category='success')
+        if request.form['submit_button'] == 'create trip':
+            date_in = request.form.get('tripDate')
+            time_in = request.form.get('tripTime')
+            name_in = request.form.get('name')
+            desc_in = request.form.get('desc')
+            tripType_in = request.form.get('tripType')
+            num_people_in = request.form.get('num_people')
+            date_time = datetime.strptime(date_in + " " + time_in,"%Y-%m-%d %H:%M")
+            if len(name_in) < 1:
+                flash('Trip name is too short!', category='error')
+            else:
+                new_trip = Trip(name = name_in, desc = desc_in, trip_type = tripType_in, date = date_time, num_people = num_people_in, user_id=current_user.id)
+                db.session.add(new_trip)
+                db.session.commit()
+                flash('Trip added!', category='success')
+
     return render_template("events.html", user=current_user)
 
 @views.route('/profile/', methods = ['GET', 'POST'])
