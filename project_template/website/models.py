33612a697d6366c542aa320c2507d152fb1943ff
mmarcu10 <mmarcu10@vols.utk.edu>
2022-10-24 11:12:24 -0400

    almost done with map implimentation

--- project_template/website/models.py
+++ project_template/website/models.py
@@ -1,18 +1,20 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from datetime import datetime
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     desc = db.Column(db.String(10000))
-    trip_type = db.Column(db.String(50))
+    trip_type = db.Column(db.Integer)
     date = db.Column(db.DateTime())
     num_people = db.Column(db.Integer)
     #how to associate different imformation with different users
     #do this in the form of a foregin key
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
