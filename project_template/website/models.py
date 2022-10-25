feba5f1f452db91b812b986e7349306de70ed629
mmarcu10 <mmarcu10@vols.utk.edu>
2022-10-24 23:59:58 -0400

    issues #36 and #35

--- project_template/website/models.py
+++ project_template/website/models.py
@@ -1,7 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-from datetime import datetime
+
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
